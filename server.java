50bcc4df9f673e1844226b6bdec32b33a881f695
ShreyasShet <shetshreyas2005@gmail.com>
2026-08-09 23:00:29 +0530

    feat: implement basic HTTP server

---
+++ server.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class server{
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from Task Manager";
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            try (OutputStream stream = exchange.getResponseBody()){
+                stream.write(response.getBytes());
+            }
+        });
+
+        server.start();
+
+        System.out.println("Server running on port: 8080");
+    }
+}
