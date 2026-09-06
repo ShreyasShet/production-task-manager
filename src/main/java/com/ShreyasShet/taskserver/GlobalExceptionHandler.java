4295e5ed2a21ad3275035a7a6ea824faff6db166
ShreyasShet <shetshreyas2005@gmail.com>
2026-09-06 08:22:53 +0530

    create a GlobalExceptionHandler to intercept exceptions

---
+++ src/main/java/com/ShreyasShet/taskserver/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.ShreyasShet.taskserver;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError ->
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
