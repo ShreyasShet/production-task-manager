c1b1fdc5a75a2130b99c9369f251d014a35ed3c8
ShreyasShet <shetshreyas2005@gmail.com>
2026-09-06 08:20:54 +0530

    add bean validation to ensureclient request is valid

--- src/main/java/com/ShreyasShet/taskserver/TaskController.java
+++ src/main/java/com/ShreyasShet/taskserver/TaskController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -30,14 +32,14 @@ public class TaskController {
     public ResponseEntity<Task> listTaskById(@PathVariable int id){
         Optional<Task> maybeTask = taskRepository.getTaskById(id);
         if(maybeTask.isPresent())
-            return ResponseEntity.ok(maybeTask.get());
+            return ResponseEntity.ok(maybeTask.get()); //200 + body
         else
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.notFound().build(); //404 + body
     }
 
     @PostMapping
-    public ResponseEntity<Task> addNewTask(@RequestBody Task task){
+    public ResponseEntity<Task> addNewTask(@Valid @RequestBody Task task){
         Task created = taskRepository.addTask(task);
-        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created); //201 + body
     }
 }
